0ea62b4e1ede47ceb167029ea30b412573dbbcb4
Ioneovv <124574758+Ioneovv@users.noreply.github.com>
2024-08-27 16:40:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,17 +13,12 @@ def load_recipes():
     try:
         with open('recipes.json', 'r') as f:
             recipes = json.load(f)
-        if not recipes:
-            print("Файл рецептов пуст.")
         return recipes
     except json.JSONDecodeError as e:
         print(f"Ошибка при чтении JSON файла: {e}")
         return []
     except FileNotFoundError as e:
         print(f"Файл не найден: {e}")
-        return []
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
         return []
 
 recipes = load_recipes()
@@ -32,34 +27,26 @@ recipes = load_recipes()
 async def send_recipe(update: Update, context: CallbackContext):
     if recipes:
         recipe = random.choice(recipes)
-        message = (
-            f"Название: {recipe.get('title', 'Неизвестно')}\n"
-            f"Ингредиенты: {recipe.get('ingredients', 'Неизвестно')}\n"
-            f"Инструкция: {recipe.get('instructions', 'Неизвестно')}"
-        )
+        message = f"Название: {recipe.get('title', 'Неизвестно')}\nИнгредиенты: {recipe.get('ingredients', 'Неизвестно')}\nИнструкция: {recipe.get('instructions', 'Неизвестно')}"
         await update.message.reply_text(message)
     else:
         await update.message.reply_text("Рецепты не загружены или файл пуст.")
 
 # Основная функция
 async def main():
-    if not TELEGRAM_BOT_TOKEN:
-        raise ValueError("Отсутствует токен бота. Убедитесь, что TELEGRAM_BOT_TOKEN установлен в переменных окружения.")
-    
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
     application.add_handler(CommandHandler("recipe", send_recipe))
 
     # Запуск бота
-    try:
-        await application.run_polling()
-    except Exception as e:
-        print(f"Произошла ошибка при запуске бота: {e}")
+    await application.run_polling()
 
 if __name__ == '__main__':
-    try:
-        asyncio.get_event_loop().run_until_complete(main())
-    except RuntimeError as e:
-        if str(e) == 'This event loop is already running':
-            print("Цикл событий уже запущен.")
-        else:
-            raise e
+    # Получаем текущий цикл событий
+    loop = asyncio.get_event_loop()
+    # Запускаем основной код, если цикл событий еще не запущен
+    if not loop.is_running():
+        loop.run_until_complete(main())
+    else:
+        # Если цикл событий уже запущен, запускаем основную функцию с помощью asyncio.create_task
+        asyncio.create_task(main())
