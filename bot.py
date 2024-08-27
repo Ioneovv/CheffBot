766e3627f8d506694aea33c18e803121a5522156
Ioneovv <124574758+Ioneovv@users.noreply.github.com>
2024-08-27 17:42:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
-import asyncio
+from telegram import Update
 from telegram.ext import Application, CommandHandler
 
-async def start(update, context):
-    await update.message.reply_text("Привет, мир!")
+# Убираем async
+def start(update: Update, context):
+    update.message.reply_text("Привет! Я бот.")
 
-async def main():
-    # Создание экземпляра приложения с токеном вашего бота
-    application = Application.builder().token('6953692387:AAEm-p8VtfqdmkHtbs8hxZWS-XNkdRN2lRE').build()
-    
-    # Добавление обработчика команды /start
-    application.add_handler(CommandHandler('start', start))
-    
-    # Запуск бота
-    await application.run_polling()
+def main():
+    application = Application.builder().token("6953692387:AAEm-p8VtfqdmkHtbs8hxZWS-XNkdRN2lRE").build()
+
+    # Убираем async
+    application.add_handler(CommandHandler("start", start))
+
+    # Убираем await и запускаем обычным способом
+    application.run_polling()
 
 if __name__ == '__main__':
-    # Получение текущего цикла событий
-    loop = asyncio.get_event_loop()
-    try:
-        # Запуск основного метода
-        loop.run_until_complete(main())
-    finally:
-        # Закрытие цикла событий, если он был закрыт
-        if loop.is_running():
-            loop.stop()
-        loop.close()
+    main()
