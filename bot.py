fb528912e70e8e1a15c59da400957db3b9c868b3
Ioneovv <124574758+Ioneovv@users.noreply.github.com>
2024-10-04 13:23:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -214,6 +214,11 @@ async def main():
     await application.run_polling()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    application = ApplicationBuilder().token("6953692387:AAEm-p8VtfqdmkHtbs8hxZWS-XNkdRN2lRE").build()  # Замените на свой токен
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(category_button, pattern=r'category_'))
+    application.add_handler(CallbackQueryHandler(recipe_button, pattern=r'recipe_'))
+    application.add_handler(CallbackQueryHandler(search_recipes, pattern='search_recipes'))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    application.run_polling()
