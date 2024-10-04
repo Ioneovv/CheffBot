61701b48e5f37747e925030dbf6e305be619b4d4
Ioneovv <124574758+Ioneovv@users.noreply.github.com>
2024-10-04 15:46:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
-import asyncio
 from telegram.ext import ApplicationBuilder
 
 async def main():
     app = ApplicationBuilder().token("6953692387:AAEm-p8VtfqdmkHtbs8hxZWS-XNkdRN2lRE").build()
     await app.initialize()
-    await app.run_polling()  # Замените на этот метод
+    await app.run_polling()  # Запуск polling
     await app.idle()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()  # просто вызовите main без asyncio.run()
